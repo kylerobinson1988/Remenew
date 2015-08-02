7a533ef5517d6185f0a594909c9229959edaba13
Sinkingstarship <kylerobinson1988@gmail.com>
2015-08-02 16:42:45 -0400

    Creation of Remenew.

---
+++ Remenew/OrderSingleton.swift
@@ -0,0 +1,13 @@
+//
+//  OrderSingleton.swift
+//  Remenew
+//
+//  Created by Kyle Brooks Robinson on 8/2/15.
+//  Copyright (c) 2015 Kyle Brooks Robinson. All rights reserved.
+//
+
+import UIKit
+
+class OrderSingleton: NSObject {
+   
+}
